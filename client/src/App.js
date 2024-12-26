e9f66880b4ec7c60faf5a13cd7b67afe665ac6a9
AleMazzaforte <109003400+AleMazzaforte@users.noreply.github.com>
2024-12-26 00:43:36 -0300

    Commit 2
    Correccion url del back  y estilos de boostrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,43 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const url = "https://rmareact.onrender.com" || "http://localhost:8080";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <label> Nombre: <input type= 'text'></input></label><br/>
+    <div className="container">
+      <div className="App">
+        
 
-        <button onClick = { async () => {
-          const res = await fetch('http://localhost:8080')
-          const data = await res.json()
-          console.log(data)
-        }} ></button>
+        
+      </div>
+      <div class="card text-center">
+  <div class="card-header">
+    Login
+  </div>
+    <div class="card-body">
+      <h5 class="card-title">Ingrese su usuario y contraseña</h5><br/>
+      
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">Usuario:</span>
+        <input type="text" class="form-control"  aria-label="Username" aria-describedby="basic-addon1"></input>
+      </div>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">Contraseña:</span>
+        <input type="password" class="form-control"  aria-label="Username" aria-describedby="basic-addon1"></input>
       </div>
       
+    </div>
+    <div class="card-footer text-body-secondary">
+    <button className="btn btn-primary" onClick = { async () => {
+          const res = await fetch(url)
+          const data = await res.json()
+          console.log(data)
+        }} >Aceptar</button>
+    </div>
+  </div>
     </div>
   );
 }
